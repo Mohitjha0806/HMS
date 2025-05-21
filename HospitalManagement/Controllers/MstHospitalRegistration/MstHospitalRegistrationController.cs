a79ba612eb168ba12dac0add905942e9d9336cae
Mohit Jha <mohitjha95844@gmail.com>
2025-05-21 14:13:02 +0530

    Add project files.

---
+++ HospitalManagement/Controllers/MstHospitalRegistration/MstHospitalRegistrationController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HospitalManagement.Web.Controllers.MstHospitalRegistration
+{
+    public class MstHospitalRegistrationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
---
+++ HospitalManagement/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc.Razor;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services
+builder.Services.AddControllersWithViews();
+
+// Configure Razor View Engine Options
+builder.Services.Configure<RazorViewEngineOptions>(options =>
+{
+    options.ViewLocationFormats.Clear();
+    options.ViewLocationFormats.Add("/Views/{1}/{0}" + RazorViewEngine.ViewExtension);
+    options.ViewLocationFormats.Add("/Views/Shared/{0}" + RazorViewEngine.ViewExtension);
+    options.ViewLocationFormats.Add("/Views/MstHospitalRegistration/HospitalRegistration/{0}" + RazorViewEngine.ViewExtension);
+});
+
+var app = builder.Build();
+
+// Configure middleware
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapStaticAssets();
+app.Run();
